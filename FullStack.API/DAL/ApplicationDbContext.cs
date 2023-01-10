1da920419e6a7c92135e979ea51df09e2055ac2e
WtfMontag <fahrenheit843f@gmail.com>
2023-01-10 15:24:19 +0300

    Добавьте файлы проекта.

---
+++ FullStack.API/DAL/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using FullStack.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullStack.API.DAL
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected ApplicationDbContext()
+        {
+            Database.EnsureCreated(); 
+        }
+
+        DbSet<Test> Tests { get; set; }
+    }
+}
---
+++ FullStack.API/Models/Test.cs
@@ -0,0 +1,8 @@
+﻿namespace FullStack.API.Models
+{
+    public class Test
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
