98b2fdaec58b8311872d7a6424b2284d21fca2b0
Vadim <6ddd14@gmail.com>
2023-01-17 13:50:26 +0300

    Add CRUD

--- LibraryAggregator.API/Controllers/AuthorController.cs
+++ LibraryAggregator.API/Controllers/AuthorController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using LibraryAggregator.DataLayer;
 using LibraryAggregator.DataLayer.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication;
 
 namespace LibraryAggregator.API.Controllers
 {
@@ -25,6 +26,50 @@ namespace LibraryAggregator.API.Controllers
         public async Task<ActionResult<IEnumerable<Author>>> Get()
         {
             return await _db.Authors.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<Author> Get(int id)
+        {
+            var Author = _db.Authors.FirstOrDefault(x => x.AuthorId == id);
+            return Author;
+        }
+
+        [HttpPost]
+        public IActionResult Post(Author author)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Authors.Add(author);
+                _db.SaveChanges();
+                return Ok(author);
+
+            }
+            return BadRequest(ModelState);
+        }
+
+        [HttpPut]
+        public IActionResult Put(Author author)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Update(author);
+                _db.SaveChanges();
+                return Ok(author);
+            }
+            return BadRequest(author);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var author = _db.Authors.FirstOrDefault(item => item.AuthorId == id);
+            if (author != null)
+            {
+                _db.Authors.Remove(author);
+                _db.SaveChanges();
+            }
+            return Ok(author);
         }
     }
 }
