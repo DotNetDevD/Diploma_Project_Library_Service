671e101ab018029181ce4e3a5aa3a3a45107c2bf
WtfMontag <fahrenheit843f@gmail.com>
2023-02-02 16:39:28 +0300

    correct test component library

--- LibraryAggregator.WebClient/src/app/test/test.component.ts
+++ LibraryAggregator.WebClient/src/app/test/test.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class TestComponent {
     data = [
-        { img: "https://therichpost.com/wp-content/uploads/2021/05/bootstrap5-carousel-slider-img1.jpg", title: "Slide 1" },
+        { img: "http://content.onliner.by/news/2014/07/default/7fb79907ab333a5679b1fecde955f22d.jpg", title: "Slide 1" },
         { img: "https://therichpost.com/wp-content/uploads/2021/05/bootstrap5-carousel-slider-img2.jpg", title: "Slide 2" },
         { img: "https://therichpost.com/wp-content/uploads/2021/05/bootstrap5-carousel-slider-img3.jpg", title: "Slide 3" }
     ];
