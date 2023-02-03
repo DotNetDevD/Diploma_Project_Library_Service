876da85c6019cd7dace07761f11c58298a54c93f
Vadim <6ddd14@gmail.com>
2023-02-03 16:35:42 +0300

    add

--- LibraryAggregator.WebClient/src/app/book/Models/BooksGener.model.ts
+++
@@ -1,8 +0,0 @@
-import { Book } from "./Book.model";
-import { Genre } from "./Genre.model";
-export interface BooksGenre{
-    BookId:number;
-    Book:Book;
-    GenreId:number;
-    Genre: Genre;
-} 
--- LibraryAggregator.WebClient/src/app/book/Models/Genre.model.ts
+++
@@ -1,7 +0,0 @@
-import { BooksGenre } from "./BooksGener.model";
-
-export interface Genre {
-    genreId: number;
-    BooksGener:BooksGenre[];
-    type:string;
-}
