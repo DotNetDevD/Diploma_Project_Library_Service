e53d0bb59f58791d5cf5bb5ab4bb7f862d053269
Vadim <6ddd14@gmail.com>
2023-02-05 19:58:13 +0300

    BooksCompleted

---
+++ LibraryAggregator.WebClient/src/app/models/booksGenres.ts
@@ -0,0 +1,12 @@
+import { Book } from "./book";
+import { Genre } from "./genre";
+
+export interface BooksGenres{
+  booksGenresId:number;
+  bookId:number;
+  genresId:number;
+  book:Book;
+  genre:Genre;
+  genreId:number;
+  type:string;
+}
